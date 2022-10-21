18497f2a9aa30da74575a7eafdc1da5521741c13
jcomeme <omemeankohji@gmail.com>
2022-10-21 11:32:13 +0900

    米追加

--- app/Library/TranslationCore.php
+++ app/Library/TranslationCore.php
@@ -9,6 +9,27 @@ class TranslationCore
     private $api_key = '';
     public $lang = [];
 
+    /*
+     * 'ja' => '日本語',
+     * 'ar'=>'アラビア語',
+     *'it'=>'イタリア語',
+     *'id'=>'インドネシア語',
+     *'nl'=>'オランダ語',
+     *'sv'=>'スウェーデン語',
+     *'es'=>'スペイン語',
+     *'th'=>'タイ語',
+     *'de'=>'ドイツ語',
+     *'tr'=>'トルコ語',
+     *'fr'=>'フランス語',
+     *'vi'=>'ベトナム語',
+     *'pt'=>'ポルトガル語',
+     *'ms'=>'マレー語',
+     *'ru'=>'ロシア語',
+     *'en'=>'英語',
+     *'zh'=>'中国語簡体',
+     *'zh-TW'=>'中国語繁体',
+     *'ko'=>'韓国語',
+    * */
     public function __construct()
     {
         $this->api_key = config("app.google_api_key");
@@ -30,7 +51,7 @@ class TranslationCore
     {
         $translate = new TranslateClient(['key' => $this->api_key]);
         $rst = [];
-        foreach ($this->lang as $key=>$value) {
+        foreach ($this->lang as $key => $value) {
             try {
                 $result = $translate->translate(
                     $text,
@@ -50,8 +71,7 @@ class TranslationCore
     public function executeTransrationMulti($text_list, $lang_code)
     {
 
-        try
-        {
+        try {
             $translate = new TranslateClient(['key' => $this->api_key]);
             $result = $translate->translateBatch(
                 $text_list,
@@ -61,9 +81,7 @@ class TranslationCore
             //\Log::debug(print_r($result,1));
 
             return $result;
-        }
-        catch (\Exception $e)
-        {
+        } catch (\Exception $e) {
             //dd($e);
             return false;
             // エラー処理
